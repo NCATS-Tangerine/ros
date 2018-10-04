504c0885fd1cf2b51d8bbd5d0f3f994c3c606e93
Steven Cox <scox@renci.org>
2018-10-04 19:04:45 -0400

    restructure

---
+++ etc/setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Environment dependent settings:
+
+export APP_ROOT=${APP_ROOT:-~/dev/reasoner-tools}
+export APP_PORT=5006
+export NUM_WORKERS=3
+#export BROKER_HOST=localhost
+#export RESULTS_HOST=localhost
