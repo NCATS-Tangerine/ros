22d7638f521ed08c896d4fcaf205b87c656c5bfd
Colin Curtis <ckcurtis@renci.org>
2019-02-20 12:35:26 -0500

    rolled back lines 127 - 131

--- ros/wf5/roscwlapi.py
+++ ros/wf5/roscwlapi.py
@@ -77,13 +77,27 @@ class LifeCycle:
                     #print (f"rendered template: {template.render (n)}")
                     questions.append (json.loads(template.render (n)))
 
-                    # print()
-                    # print('questions:', questions)
-                    # print()
-
-
-
         #print (f"questions: {json.dumps(questions, indent=2)}")
+
+        print()
+
+        drug_names_from_questions = []
+
+        # for completed_template in questions:
+        #     print (json.dumps(completed_template, indent=2))
+        #     print()
+        #     for key, value in completed_template['machine_question']['nodes'][0].items():
+        #         if key == 'name':
+        #             if value in drug_names_from_questions:
+        #                 continue
+        #             else:
+        #                 drug_names_from_questions.append(value)
+        #             print('key:', key)
+        #             print('value:', value)
+        #             print()
+        # #print(questions['name'])
+        # #print(questions)
+        # print('drug names from questions:', drug_names_from_questions)
         return questions
     
     def execute (self, question_path, source_path, query, filter_val, service, output):
@@ -100,10 +114,9 @@ class LifeCycle:
         else:
             questions = [ question ]
         
-        #print(questions)
-        
         op = GraphOp (url = service)
 
+        all_responses = []
         response = {}
 
         for q in questions:
@@ -111,31 +124,32 @@ class LifeCycle:
             response0 = op.call (q)
             #print (f"response0 {json.dumps(response0, indent=2)}")
 
-            # print()
-            # print('response0:', response0)
-            # print()
-            # print('length of response0:', len(response0))
-            # print()
-            # print('response0 type:', type(response0))
-            # print()
             if isinstance(response0, dict):
-                response.update (response0)
+                #response.update (response0)
+                #response.update(response0)
+                #all_responses.append(response)
+                response.update(response0)
+                print('response0 added to all_responses')
             else:
                 print('skipping this response0, it is of the wrong type!')
-
+        #print(json.dumps(all_responses, indent=2))
         
         ''' Record the response. '''
 
         #print (f"output: {output}")
-        if isinstance(output, TextIOWrapper):
-            with open(output.name, "w") as stream:
-                #print (f"response: {response} and ")
+        for response in all_responses:
+        
+            if isinstance(output, TextIOWrapper):
+                with open(output.name, "a") as stream:
+                    #print (f"response: {response} and ")
+                    if response:
+                        
+                        json.dump (response, stream, indent=2)
+            else:
                 if response:
-                    
-                    json.dump (response, stream, indent=2)
-        else:
-            if response:
-                json.dump (response, output, indent=2)
+                    json.dump (response, output, indent=2)
+            print(len(response))
+        print('all responses length:', len(all_responses))
         
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(
